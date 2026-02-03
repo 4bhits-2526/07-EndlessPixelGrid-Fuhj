0ca53a4b9f71b734a26ceface0b43770c0afc3db
Jonah FUhrmeister <jonah.fuhrmeister08@htl-salzburg.ac.at>
2026-02-03 14:18:32 +0100

    Apply FIFO shift to grid data on commit

--- Assets/MyGame/Skripts/DataModel.cs
+++ Assets/MyGame/Skripts/DataModel.cs
@@ -7,4 +7,31 @@ public class DataModel : MonoBehaviour
 
     // Raster: 10 Zeilen, 7 Spalten
     public bool[,] grid = new bool[10, 7];
+
+    // -----------------------------
+    // Feature 7: FIFO-Shift der Eingabezeile ins Raster
+    // -----------------------------
+    public void ShiftInputLineToGrid()
+    {
+        // 1. Alle Zeilen nach oben verschieben
+        for (int row = 0; row < grid.GetLength(0) - 1; row++)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                grid[row, col] = grid[row + 1, col];
+            }
+        }
+
+        // 2. Eingabezeile unten einfügen
+        for (int col = 0; col < grid.GetLength(1); col++)
+        {
+            grid[grid.GetLength(0) - 1, col] = inputRow[col];
+        }
+
+        // 3. Eingabezeile zurücksetzen
+        for (int col = 0; col < inputRow.Length; col++)
+        {
+            inputRow[col] = false;
+        }
+    }
 }
--- Assets/MyGame/Skripts/InputToggleController.cs
+++ Assets/MyGame/Skripts/InputToggleController.cs
@@ -3,41 +3,45 @@ using UnityEngine.UI;
 
 public class InputToggleController : MonoBehaviour
 {
-    public DataModel dataModel;      // Referenz auf Feature 5
-    public Image[] inputImages;      // 7 UI Images (Q6 → Q0)
+    public DataModel dataModel;      // Referenz auf dein bestehendes DataModel
+    public Image[] inputImages;      // 7 UI-Images für die Eingabezeile (Q6 → Q0)
 
     void Update()
     {
-        // Q6 (links)
-        if (Input.GetKeyDown(KeyCode.Q))
-            TogglePixel(6);
+        // -----------------------------
+        // Feature 6: Pixel-Toggles Q-U
+        // -----------------------------
+        if (Input.GetKeyDown(KeyCode.Q)) TogglePixel(6);
+        if (Input.GetKeyDown(KeyCode.W)) TogglePixel(5);
+        if (Input.GetKeyDown(KeyCode.E)) TogglePixel(4);
+        if (Input.GetKeyDown(KeyCode.R)) TogglePixel(3);
+        if (Input.GetKeyDown(KeyCode.T)) TogglePixel(2);
+        if (Input.GetKeyDown(KeyCode.Y)) TogglePixel(1);
+        if (Input.GetKeyDown(KeyCode.U)) TogglePixel(0);
 
-        if (Input.GetKeyDown(KeyCode.W))
-            TogglePixel(5);
-
-        if (Input.GetKeyDown(KeyCode.E))
-            TogglePixel(4);
-
-        if (Input.GetKeyDown(KeyCode.R))
-            TogglePixel(3);
-
-        if (Input.GetKeyDown(KeyCode.T))
-            TogglePixel(2);
-
-        if (Input.GetKeyDown(KeyCode.Y))
-            TogglePixel(1);
-
-        if (Input.GetKeyDown(KeyCode.U))
-            TogglePixel(0);
+        // -----------------------------
+        // Feature 7: FIFO-Shift D
+        // -----------------------------
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            dataModel.ShiftInputLineToGrid(); // ruft die neue Funktion in DataModel auf
+            UpdateInputUI(); // UI der Eingabezeile zurücksetzen
+        }
     }
 
     void TogglePixel(int index)
     {
-        // bool toggeln
+        // Pixel toggeln
         dataModel.inputRow[index] = !dataModel.inputRow[index];
+        UpdateInputUI();
+    }
 
-        // UI aktualisieren
-        inputImages[index].color =
-            dataModel.inputRow[index] ? Color.white : Color.black;
+    void UpdateInputUI()
+    {
+        // Eingabezeilen-UI aktualisieren (weiß = an, schwarz = aus)
+        for (int i = 0; i < inputImages.Length; i++)
+        {
+            inputImages[i].color = dataModel.inputRow[i] ? Color.white : Color.black;
+        }
     }
 }
